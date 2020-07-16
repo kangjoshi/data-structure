dae678dccdf8c841f746af3d064d929c0f027030
sungjin <kangjoshi@gmail.com>
2020-07-16 11:11:43 +0900

    1. SymbolTable

---
+++ src/main/java/SequentialSymbolTable.java
@@ -0,0 +1,85 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * 비순차 연결 리스트 심볼 테이블
+ * */
+public class SequentialSymbolTable<K, V> {
+
+    private int size;
+    private Node first;
+
+    private class Node {
+        K key;
+        V value;
+        Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public K[] keys() {
+        if (size == 0) return null;
+
+        @SuppressWarnings("unchecked")
+        K[] keys = (K[]) Array.newInstance(first.key.getClass(), size);
+
+        int index = 0;
+        for(Node n=first; n!=null; n=n.next) {
+            keys[index++] = n.key;
+        }
+
+        return keys;
+    }
+
+    public boolean put(K key, V value) {
+        for (Node n=first; n!=null; n=n.next) {
+            if (key.equals(n.key)) {
+                n.value = value;
+                return true;
+            }
+        }
+        first = new Node(key, value, first);
+        size++;
+        return true;
+    }
+
+    public V get(K key) {
+        if (size == 0) return null;
+
+        V result = null;
+        for (Node n=first; n!=null; n=n.next) {
+            if (key.equals(n.key)) {
+                result = n.value;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public boolean delete(V key) {
+        if (size == 0) return false;
+
+        Node last = null;
+        for (Node n=first; n!=null; n=n.next) {
+            if (key.equals(n.key)) {
+                n = null;
+                if (last != null)
+                    last.next = null;
+                size--;
+                return true;
+            }
+            last = n;
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/SequentialSymbolTableTest.java
@@ -0,0 +1,117 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SequentialSymbolTableTest{
+
+    /*
+     * TDD 시나리오
+     *   1. Key(String) & Value(String)를 받는 SymbolTable 클래스 생성
+     *   2. 값을 입력하는 put 메서드 생성 (저장 되었으면 true 반환)
+     *   3. 값을 반환하는 get 메서드 생성 (없으면 null 반환)
+     * */
+
+    @Test
+    public void whenCreateSymbolTableThenCreateSymbolTable(){
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+
+        assertNotNull(symbolTable);
+    }
+
+    @Test
+    public void whenPutKeyAndValueThenReturnTrue() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+
+        boolean result = symbolTable.put("key", "value");
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void whenGetValueFromEmptyTableThenReturnNull() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+
+        String result = symbolTable.get("invalid");
+
+        assertNull(result);
+    }
+
+    @Test
+    public void whenGetValueByWrongKeyThenReturnNull() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+        symbolTable.put("key", "value");
+
+        String result = symbolTable.get("invalid");
+
+        assertNull(result);
+    }
+
+    @Test
+    public void whenGetValueByKeyThenReturnValue() {
+        String value = "value";
+
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+        symbolTable.put("key", value);
+        String result = symbolTable.get("key");
+
+        assertEquals(value, result);
+        assertEquals(symbolTable.size(), 1);
+    }
+
+    @Test
+    public void whenGetSizeThenReturnSize() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+        symbolTable.put("key1", "value");
+        symbolTable.put("key2", "value");
+
+        int size = symbolTable.size();
+
+        SequentialSymbolTable<String, String> duplicateSymbolTable = new SequentialSymbolTable<>();
+        duplicateSymbolTable.put("sameKey", "value");
+        duplicateSymbolTable.put("sameKey", "value");
+
+        int duplicateSize = duplicateSymbolTable.size();
+
+        assertEquals(size, 2);
+        assertEquals(duplicateSize, 1);
+    }
+
+    @Test
+    public void whenGetAllKeysThenReturnAllKeys() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+        symbolTable.put("key1", "value");
+        symbolTable.put("key2", "value");
+
+        String[] keys = symbolTable.keys();
+
+        assertEquals(keys.length, 2);
+    }
+
+    @Test
+    public void whenDeleteByWrongKeyThenReturnFalse() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+
+        String key = "invalid";
+        boolean result = symbolTable.delete(key);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void whenDeleteByKeyThenReturnTrueAndDecreaseSize() {
+        SequentialSymbolTable<String, String> symbolTable = new SequentialSymbolTable<>();
+        symbolTable.put("key1", "value");
+        symbolTable.put("key2", "value");
+
+        int beforeSize = symbolTable.size();
+
+        boolean result = symbolTable.delete("key1");
+
+        String value = symbolTable.get("key1");
+
+        assertTrue(result);
+        assertEquals(symbolTable.size(), beforeSize - 1);
+        assertNull(value);
+    }
+}
